3428007aad0cf2b9a22aaf21971ee01a0aa5166c
chen_you <chin_you@163.com>
2017-12-04 14:49:01 +0800

    修改代码格式

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import Btn from './components/btn/index.js'
 import Container from './components/container/index.js'
 import Row from './components/row/index.js'
 import Col from './components/col/index.js'
+
 const components = [
   HelloWorld,
   Carrousel3d,
